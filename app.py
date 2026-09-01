a3c45a2481cb41ce5449674c976e7f0bdd832ac4
koushil-AI-ML <koushiljakka07@gmail.com>
2026-09-02 01:01:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def predict():
     return jsonify({"extracted_text": text, "matches": results})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
